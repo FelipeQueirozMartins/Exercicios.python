796b35ed468dd3dd4126db0fe1de5539d503f622
Felipe de Queiroz Martins <fefeipo47@gmail.com>
2026-08-28 09:59:28 -0300

    Exercício Aula 4
    Exercício simples de apresentação de python do curso na aula 4

---
+++ front.py
@@ -0,0 +1,4 @@
+nome = input ("qual o seu nome? ")
+idade = input (" qual a sua idade? ")
+peso = input ('qual o seu peso aproximado? ')
+print ("prazer,", nome, 'legal que voce tenha', idade, 'anos, vc pesa so isso? ', peso, "kg?")
