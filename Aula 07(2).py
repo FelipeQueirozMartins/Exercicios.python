56bc8aadb46a5b28ff2425ab9a0a70c1c30e1d8e
Felipe de Queiroz Martins <fefeipo47@gmail.com>
2026-09-08 11:56:16 -0300

    Add files via upload
    Exercício simples sobre dobro, triplo e raiz.

---
+++ Aula 07(2).py
@@ -0,0 +1,7 @@
+N = int(input('Digite um número: '))
+D = N * 2
+T = N * 3
+RQ = N ** (1/2)
+print ('O dobro de {} é {}'.format (N, D))
+print ('O triplo de {} é {}'.format (N, T))
+print ('A raiz Quadrada de {} é {:.2f}'.format (N, RQ))
