72e91ac36ec0dd13f4fc647db5a81c811673c1af
Felipe de Queiroz Martins <fefeipo47@gmail.com>
2026-09-16 09:46:03 -0300

    Add files via upload
    Exercício para construir uma tabuada simples

---
+++ Aula 07(Tabuada).py
@@ -0,0 +1,23 @@
+T = int(input('Digite o número que deseja ver a tabuada: '))
+print('-----------')
+T1 = T * 1
+T2 = T * 2
+T3 = T * 3
+T4 = T * 4 
+T5 = T * 5
+T6 = T * 6
+T7 = T * 7
+T8 = T * 8
+T9 = T * 9
+T10 = T * 10
+print('{} X 1 = {}'.format(T, T1))
+print('{} X 2 = {}'.format(T, T2))
+print('{} X 3 = {}'.format(T, T3))
+print('{} X 4 = {}'.format(T, T4))
+print('{} X 5 = {}'.format(T, T5))
+print('{} X 6 = {}'.format(T, T6))
+print('{} X 7 = {}'.format(T, T7))
+print('{} X 8 = {}'.format(T, T8))
+print('{} X 9 = {}'.format(T, T9))
+print('{} X 10 = {}'.format(T, T10))
+print('-----------')
