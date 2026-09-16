c10fbfc6e3954fdd010cd35a8f53531b31cc0977
Felipe de Queiroz Martins <fefeipo47@gmail.com>
2026-09-16 08:40:33 -0300

    Add files via upload
    Exercício simples em que calcula a media aritmética do aluno

---
+++ Aula 07(Media Aritimetica).py
@@ -0,0 +1,7 @@
+N1 = float(input('Digite a Primeira nota do aluno '))
+N2 = float(input('Digite a segunda nota do aluno '))
+N3 = float(input('Digite a terceira nota do aluno '))
+N4 = float(input('digite a quarta nota do aluno '))
+RS = N1 + N2 + N3 + N4
+MA = RS / 4 
+print('A media final do aluno é: {:.1f}'.format(MA))
